2140e640d6c0804811c77bcb028946c8b9987f07
Enrique Barragan <ebarragan1997@gmail.com>
2019-12-06 14:52:56 -0600

    Update dataset

--- labeling.py
+++ labeling.py
@@ -24,13 +24,15 @@ train_labels = labels
 data_order = 'tf'  # 'th' for Theano, 'tf' for Tensorflow
 # check the order of data and chose proper data shape to save images
 if data_order == 'th':
-    train_shape = (len(train_addrs), 3, 75, 75)
+    train_shape = (len(train_addrs), 3, 64, 64)
 elif data_order == 'tf':
-    train_shape = (len(train_addrs), 75, 75, 3)
+    train_shape = (len(train_addrs), 64, 64, 3)
 
 # open a hdf5 file and create earrays
 hdf5_file = h5py.File(hdf5_path, mode='w')
-hdf5_file.create_dataset("train_img", train_shape, np.int8)
+hdf5_file.create_dataset("train_img", train_shape, np.uint8,
+                         compression="gzip",
+                         compression_opts=9)
 hdf5_file.create_dataset("train_mean", train_shape[1:], np.float32)
 hdf5_file.create_dataset("train_labels", (len(train_addrs),), np.int8)
 hdf5_file["train_labels"][...] = train_labels
@@ -38,21 +40,21 @@ hdf5_file["train_labels"][...] = train_labels
 mean = np.zeros(train_shape[1:], np.float32)
 # loop over train addresses
 for i in range(len(train_addrs)):
-    # print how many images are saved every 750 images
-    if i % 750 == 0 and i > 1:
-        print('train data: {}/{}'.format(i, len(train_addrs)))
-    # read an image and resize to (75, 75)
+    # print how many images are saved every 640 images
+    # if i % 64 == 0 and i > 1:
+    print('train data: {}/{}'.format(i, len(train_addrs)))
+    # read an image and resize to (64, 64)
     # cv2 load images as BGR, convert it to RGB
     addr = train_addrs[i]
     img = cv2.imread(addr)
-    img = cv2.resize(img, (75, 75), interpolation=cv2.INTER_CUBIC)
+    img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_CUBIC)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # add any image pre-processing here
     # if the data order is Theano, axis orders should change
     if data_order == 'th':
         img = np.rollaxis(img, 2)
     # save the image and calculate the mean so far
-    hdf5_file["train_img"][i, ...] = img[None]
+    hdf5_file["train_img"][i, ...] = img
     mean += img / float(len(train_labels))
 
 # save the mean and close the hdf5 file
